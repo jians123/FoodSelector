e01b4b5256c2103caa6ad048256fccc85efd1c07
jians123 <100296457+jians123@users.noreply.github.com>
2022-04-20 19:35:29 +0800

    Update food.js

--- food.js
+++ food.js
@@ -1,17 +1,26 @@
 class Food{
     constructor(_imgName){
-        this.x = 0;
-        this.y = 0;
+        this.x = random(windowWidth);
+        this.y = random(-2000);
         this.alpha = 255;
         this.img = loadImage(_imgName);
         this.state = 0;
+        this.fallSpeed = random(1,2);
+        this.rotateSpeed = random(0.005,0.01);
     }
 
-    draw(x,y){
-        image(this.img, x, y);
+    draw(){
+        push();
+        translate(this.x,this.y);
+        rotate(sin(this.rotateSpeed*frameCount)/2);
+        image(this.img, 0, 0);
+        pop();
     }
 
-    update(){
-
+    fall(){
+        this.y += this.fallSpeed;
+        if(this.y > windowHeight + 200){
+            this.y = random(-2000);
+        }
     }
 }
